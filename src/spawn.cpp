f1049e8902ddb9a0562790df9bb19d02c5252eaa
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2021-03-12 12:29:56 +0100

    spawn cleans its private params

--- src/spawn.cpp
+++ src/spawn.cpp
@@ -38,32 +38,45 @@ std::vector<int> read_color(ros::NodeHandle &node, std::string param, std::vecto
   return fallback;
 }
 
+void clearParams(ros::NodeHandle &nh)
+{
+  const auto ns(nh.getNamespace());
+  const auto length = ns.size();
+  std::vector<std::string> params;
+  nh.getParamNames(params);
+  for(const auto &param: params)
+  {
+    if(param.size() > length && param.substr(0, length) == ns)
+      nh.deleteParam(param);
+  }
+}
+
 
 using map_simulator::Spawn;
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "spawner");
-  auto node{ros::NodeHandle("~")};
+  auto nh{ros::NodeHandle("~")};
 
   // arguments reflect service call
   auto request = Spawn::Request();
 
   request.robot_namespace = ros::NodeHandle().getNamespace();
 
-  request.x = node.param("x", 0.);
-  request.y = node.param("y", 0.);
-  request.theta = node.param("theta", 0.);
-  request.radius = node.param("radius", 0.3);
+  request.x = nh.param("x", 0.);
+  request.y = nh.param("y", 0.);
+  request.theta = nh.param("theta", 0.);
+  request.radius = nh.param("radius", 0.3);
 
-  const auto shape = node.param<std::string>("shape", "circle");
+  const auto shape = nh.param<std::string>("shape", "circle");
   request.shape = shape == "square" ? request.SHAPE_SQUARE : request.SHAPE_CIRCLE;
 
-  request.linear_noise = node.param("linear_noise", 0.);
-  request.angular_noise = node.param("angular_noise", 0.);
+  request.linear_noise = nh.param("linear_noise", 0.);
+  request.angular_noise = nh.param("angular_noise", 0.);
 
-  auto robot_color = read_color(node, "robot_color", std::vector<int>{0,0,0});
-  auto laser_color = read_color(node, "laser_color", std::vector<int>{255,0,0});
+  auto robot_color = read_color(nh, "robot_color", std::vector<int>{0,0,0});
+  auto laser_color = read_color(nh, "laser_color", std::vector<int>{255,0,0});
 
   std::copy(robot_color.begin(), robot_color.end(), request.robot_color.begin());
 
@@ -71,20 +84,22 @@ int main(int argc, char** argv)
     laser_color = {255,0,0};
   std::copy(laser_color.begin(), laser_color.end(), request.laser_color.begin());
 
-  request.force_scanner = node.param("force_scanner", true);
-  request.static_tf_odom = node.param("static_tf_odom", false);
-  request.zero_joints = node.param("zero_joints", false);
+  request.force_scanner = nh.param("force_scanner", true);
+  request.static_tf_odom = nh.param("static_tf_odom", false);
+  request.zero_joints = nh.param("zero_joints", false);
 
-  auto client = node.serviceClient<Spawn>("/simulator/spawn");
+  auto client = nh.serviceClient<Spawn>("/simulator/spawn");
   client.waitForExistence();
 
   auto response = Spawn::Response();
   if(client.call(request, response))
   {
     ROS_INFO("successfully spawned robot in namespace %s", request.robot_namespace.c_str());
+    clearParams(nh);
     return 0;
   }
 
   ROS_ERROR("service call failed :(");
+  clearParams(nh);
   return 1;
 }
