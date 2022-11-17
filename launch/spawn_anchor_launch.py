09c29748097559f0928e9448546a86ff9bcf9aa6
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-11-17 14:23:42 +0100

    description in spawn_anchor

--- launch/spawn_anchor_launch.py
+++ launch/spawn_anchor_launch.py
@@ -3,14 +3,14 @@ from simple_launch import SimpleLauncher
 def generate_launch_description():
     sl = SimpleLauncher()
 
-    sl.declare_arg('frame','anchor','Name of the anchor frame')
-    sl.declare_arg('x',0.,'x-position of the anchor')
-    sl.declare_arg('y',0.,'y-position of the anchor')
-    sl.declare_arg('range_min',0.,'Min range')
-    sl.declare_arg('range_max',100.,'Max range')
-    sl.declare_arg('covariance',0.01,'covariance factor of the anchor range')
-    sl.declare_arg('covariance_real',0.01,'real covariance factor of the anchor range')
-    sl.declare_arg('publish_gt',True,'Publish real anchor position')
+    sl.declare_arg('frame','anchor',description='Name of the anchor frame')
+    sl.declare_arg('x',0.,description='x-position of the anchor')
+    sl.declare_arg('y',0.,description='y-position of the anchor')
+    sl.declare_arg('range_min',0.,description='Min range')
+    sl.declare_arg('range_max',100.,description='Max range')
+    sl.declare_arg('covariance',0.01,description='covariance factor of the anchor range')
+    sl.declare_arg('covariance_real',0.01,description='real covariance factor of the anchor range')
+    sl.declare_arg('publish_gt',True,description='Publish real anchor position')
 
     parameters = sl.arg_map(('frame','x','y','range_min','range_max','covariance','covariance_real'))
     # ensure floating points in case integers are passed
